387ab7246b0d3161073c1d52009c5bdced085ad8
joaopulsz <joaopulsz@hotmail.com>
2022-06-25 15:31:17 +0100

    refactored code

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const form = document.querySelector('#select-location');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const newLocation = document.createElement('div');
-  newLocation.className = 'location';
+  const newDiv = document.createElement('div');
+  newDiv.className = 'location';
 
   const userInput = form.elements.input.value;
 
-  const res = await fetch('https://weatherdbi.herokuapp.com/data/weather/' + userInput);
-  const weatherData = await res.json();
+  const getWeatherData = await fetch(`https://weatherdbi.herokuapp.com/data/weather/${userInput}`).json();
 
-  if (weatherData.status === 'fail') {
+  if (getWeatherData.status === 'fail' && !document.querySelector('#select-location p')) {
     const errorMsg = document.createElement('p');
     errorMsg.innerText = 'Please, enter a valid location!';
     form.appendChild(errorMsg);
@@ -23,30 +22,44 @@ form.addEventListener('submit', async (e) => {
     }
 
     const title = document.createElement('p');
-    title.innerText = weatherData.region;
+    title.innerText = getWeatherData.region;
     title.setAttribute('id', 'title');
+
+    const {
+      currentConditions: {
+        comment,
+        temp,
+        iconURL,
+        precip
+      }
+    } = getWeatherData;
+
     const condition = document.createElement('p');
-    condition.innerText = 'Weather condition: ' + weatherData.currentConditions.comment;
+    condition.innerText = 'Weather condition: ' + comment;
+
     const temperature = document.createElement('p');
-    temperature.innerText = 'Temperature: ' + weatherData.currentConditions.temp.c + ' °C';
+    temperature.innerText = 'Temperature: ' + temp.c + ' °C';
+
     const icon = document.createElement('img');
-    icon.setAttribute('src', weatherData.currentConditions.iconURL);
+    icon.setAttribute('src', iconURL);
+
     const precipitation = document.createElement('p');
-    precipitation.innerText = 'Precipitation: ' + weatherData.currentConditions.precip;
+    precipitation.innerText = 'Precipitation: ' + precip;
 
     const closeBtn = document.createElement('button');
     closeBtn.innerText = 'x';
-    newLocation.appendChild(closeBtn);
+    newDiv.appendChild(closeBtn);
+
     closeBtn.addEventListener('click', () => {
-      newLocation.remove();
+      newDiv.remove();
     });
 
-    newLocation.appendChild(title);
-    newLocation.appendChild(icon);
-    newLocation.appendChild(condition);
-    newLocation.appendChild(temperature);
-    newLocation.appendChild(precipitation);
+    newDiv.appendChild(title);
+    newDiv.appendChild(icon);
+    newDiv.appendChild(condition);
+    newDiv.appendChild(temperature);
+    newDiv.appendChild(precipitation);
 
-    main.appendChild(newLocation);
+    main.appendChild(newDiv);
   }
 });
